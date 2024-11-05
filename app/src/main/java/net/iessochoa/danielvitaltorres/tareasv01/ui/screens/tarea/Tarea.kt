62287ba90e8cd4617aec8130fcc73e393bb614e9
DanielV <deadblood404@hotmail.com>
2024-11-05 19:16:21 +0100

    Creados archivos de paquete componentes

--- app/src/main/java/net/iessochoa/danielvitaltorres/tareasv01/ui/screens/tarea/Tarea.kt
+++ app/src/main/java/net/iessochoa/danielvitaltorres/tareasv01/ui/screens/tarea/Tarea.kt
@@ -1,2 +1,18 @@
 package net.iessochoa.danielvitaltorres.tareasv01.ui.screens.tarea
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun TaskScreen(){
+    Column {
+        Row {
+            Column {
+
+            }
+        }
+    }
+}
